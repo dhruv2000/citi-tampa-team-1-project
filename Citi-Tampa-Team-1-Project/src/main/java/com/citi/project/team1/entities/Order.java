448f282921ad71bf19725c4e2352e2249c01b292
Mego MyLego <mego@yahoo.com>
2022-08-12 18:15:06 +0000

    entities added

--- Citi-Tampa-Team-1-Project/src/main/java/com/citi/project/team1/entities/Order.java
+++ Citi-Tampa-Team-1-Project/src/main/java/com/citi/project/team1/entities/Order.java
@@ -1,5 +1,103 @@
 package com.citi.project.team1.entities;
 
-public class Order {
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+
+@Entity @Table(name="order")
+
+
+
+
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    // add attributes for all the remaining properties
+    @Column(name="type") private String type;
+    @Column(name="price") private Double price;
+    @Column(name="time") private String time;
+    @Column(name="asset") private String asset;
+    @Column(name="status_code") private int status_code;
+    @Column(name="numshares") private int numshares;
+
+    public Order() {}
+
+    public Order(String type, Double price, String time, String asset, int status_code, int numshares) {
+        this.type = type;
+        this.price = price;
+        this.time = time;
+        this.asset = asset;
+        this.status_code = status_code;
+        this.numshares = numshares;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAsset() {
+        return asset;
+    }
+
+    public void setAsset(String asset) {
+        this.asset = asset;
+    }
+
+    public int getStatus_code() {
+        return status_code;
+    }
+
+    public void setStatus_code(int status_code) {
+        this.status_code = status_code;
+    }
+
+    public int getNumshares() {
+        return numshares;
+    }
+
+    public void setNumshares(int numshares) {
+        this.numshares = numshares;
+    }
+
+
+
 
 }
---
+++ Citi-Tampa-Team-1-Project/src/main/java/com/citi/project/team1/entities/Stocks.java
@@ -0,0 +1,93 @@
+package com.citi.project.team1.entities;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "stocks")
+
+
+public class Stocks implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    // add attributes for all the remaining properties
+    @Column(name = "symbol")
+    private String symbol;
+    @Column(name = "currency")
+    private String currency;
+    @Column(name = "market_time")
+    private String market_time;
+    @Column(name = "volume")
+    private int volume;
+    @Column(name = "last_price")
+    private Double last_price;
+
+    public Stocks() {}
+
+    public Stocks(int id, String symbol, String currency, String market_time, int volume, Double last_price) {
+        this.id = id;
+        this.symbol = symbol;
+        this.currency = currency;
+        this.market_time = market_time;
+        this.volume = volume;
+        this.last_price = last_price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getMarket_time() {
+        return market_time;
+    }
+
+    public void setMarket_time(String market_time) {
+        this.market_time = market_time;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public Double getLast_price() {
+        return last_price;
+    }
+
+    public void setLast_price(Double last_price) {
+        this.last_price = last_price;
+    }
+}
+
