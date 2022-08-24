c3bcf5f6dcd68a2630cecc24a230b5be93c11a5f
Dhruv Patel <dhruv2000patel@gmail.com>
2022-08-24 19:40:46 +0000

    fixed server thing

--- client/team1AngularApp/src/app/app.module.ts
+++ client/team1AngularApp/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AddOrderComponent } from './add-order/add-order.component';
+import { EditOrderComponent } from './edit-order/edit-order.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { OrderItemComponent } from './order-item/order-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddOrderComponent,
+    EditOrderComponent,
+    OrderHistoryComponent,
+    OrderItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
