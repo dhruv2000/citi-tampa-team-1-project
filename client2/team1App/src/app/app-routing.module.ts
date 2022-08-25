85f117f0fad2d6f2e086a7180a31d57f1ebcfc4b
Dhruv Patel <dhruv2000patel@gmail.com>
2022-08-25 14:21:18 +0000

    home component added

--- client2/team1App/src/app/app-routing.module.ts
+++ client2/team1App/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { AddOrderComponent } from './add-order/add-order.component';
 import { EditOrderComponent } from './edit-order/edit-order.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'addorder', component: AddOrderComponent },
   { path: 'editorders', component: EditOrderComponent },
   { path: 'viewhistory', component: OrderHistoryComponent }
